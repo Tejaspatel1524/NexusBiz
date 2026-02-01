fa6a35c68ced0dc084f3442803fd2ddc58f924b1
Tejas Patel <tejas@nexusbiz.com>
2026-02-01 22:29:40 +0530

    REMOVE: Login portal completely removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './i18n';
 import Navbar from './components/layout/Navbar';
+import Landing from './pages/Landing';
 import Generator from './pages/Generator';
 import Results from './pages/Results';
 import BusinessPlan from './pages/BusinessPlan';
 import Pricing from './pages/Pricing';
 import About from './pages/About';
 import Settings from './pages/Settings';
-import Login from './pages/Login';
 import FeatureDetail from './pages/FeatureDetail';
-import ProtectedRoute from './components/common/ProtectedRoute';
 import { useThemeStore } from './store/useThemeStore';
 
 const App: React.FC = () => {
@@ -34,18 +33,14 @@ const App: React.FC = () => {
           <Navbar />
           <main>
             <Routes>
-              {/* Public Routes */}
-              <Route path="/" element={<Navigate to="/login" replace />} />
+              <Route path="/" element={<Landing />} />
+              <Route path="/generator" element={<Generator />} />
+              <Route path="/results" element={<Results />} />
+              <Route path="/plan/:id" element={<BusinessPlan />} />
               <Route path="/pricing" element={<Pricing />} />
               <Route path="/about" element={<About />} />
-              <Route path="/login" element={<Login />} />
-
-              {/* Protected Routes - Require Authentication */}
-              <Route path="/generator" element={<ProtectedRoute><Generator /></ProtectedRoute>} />
-              <Route path="/results" element={<ProtectedRoute><Results /></ProtectedRoute>} />
-              <Route path="/plan/:id" element={<ProtectedRoute><BusinessPlan /></ProtectedRoute>} />
-              <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
-              <Route path="/feature/:featureId" element={<ProtectedRoute><FeatureDetail /></ProtectedRoute>} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/feature/:featureId" element={<FeatureDetail />} />
             </Routes>
           </main>
         </div>
